30e0c936b0521f0ab17a98473b3d7635b2d69bde
HeisukeInoue <kenbahn2002@gmail.com>
2025-12-08 16:34:51 +0900

    関数を細かく分け、コードを簡略化

--- 非同期処理課題2/script.js
+++ 非同期処理課題2/script.js
@@ -1,125 +1,93 @@
-const button = document.getElementById("search");
-const inputarea = document.getElementById("pokeID");
-const initLogMessage = document.getElementById("initiallog");
-initLogMessage.innerText = "履歴無し";
+const button = document.getElementById('search');
+const inputarea = document.getElementById('pokeID');
+const initLogMessage = document.getElementById('initiallog');
+initLogMessage.innerText = '履歴無し';
 let pokeLogs = [];
 
-if (inputarea.value === "") {
-  button.disabled = true;
+if (inputarea.value === '') {
+	button.disabled = true;
 }
 
-inputarea.addEventListener("input", () => {
-  button.disabled = false;
-  if (inputarea.value === "") {
-    button.disabled = true;
-  }
+inputarea.addEventListener('input', () => {
+	button.disabled = false;
+	if (inputarea.value === '') {
+		button.disabled = true;
+	}
 });
 
-button.addEventListener("click", () => {
-  getPokeData();
-  getPokeLog();
+button.addEventListener('click', () => {
+	getPokeData();
 });
 
 async function getPokeData() {
-  const idNumber = inputarea.value; /*入力されたID数値を格納する変数*/
-  const loadingIMG = document.createElement("img");
-  loadingIMG.classList.add("loadings");
-  loadingIMG.src = "https://usagif.com/wp-content/uploads/loading-96.gif";
-  const infos = document.getElementById("infos");
-  infos.appendChild(loadingIMG);
-  const errorMessage = document.getElementById("errorText");
-  try {
-    // await: fetchが完了するまで一時停止
-    const pokeAPI = "https://pokeapi.co/api/v2/pokemon";
-    const eachID = "/" + idNumber + "/";
-    const pokeeachID =
-      pokeAPI +
-      eachID; /*それぞれのIDポケモンのデータが個別に格納されているリンク*/
-    const response = await fetch(pokeeachID);
+	const idNumber = inputarea.value; /*入力されたID数値を格納する変数*/
+	const errorMessage = document.getElementById('errorText');
+	loadings(true);
+	try {
+		// await: fetchが完了するまで一時停止
+		const pokeAPI = `https://pokeapi.co/api/v2/pokemon/${idNumber}`;
+		const response = await fetch(pokeAPI);
 
-    if (!response.ok) {
-      // 失敗時、ここでエラーを投げる
-      throw new Error("HTTPエラー");
-    }
+		if (!response.ok) {
+			// 失敗時、ここでエラーを投げる
+			throw new Error('HTTPエラー');
+		}
+		// await: JSONの解析が完了するまで一時停止
+		loadings(false);
+		errorMessage.innerText = '';
+		const data = await response.json();
 
-    // await: JSONの解析が完了するまで一時停止
-    loadingIMG.remove();
-    errorMessage.innerText = "";
-    const data = await response.json();
-    const pokeIMG = data.sprites.front_default;
-    const pokeName = data.name;
-    const pokeID = data.id;
-    const poketypes = data.types;
-    const pokeheight = data.height * 100;
-    const pokeweight = data.weight / 100;
+		const { sprites, name, id, types, height, weight } = data;
 
-    const pokeTypes = document.getElementById("types");
-    pokeTypes.innerText = "タイプ: " + poketypes[0].type.name;
-    for (let i = 1; i < poketypes.length; i++) {
-      pokeTypes.innerText += "," + poketypes[i].type.name;
-    }
+		const pokeTypesElement = document.getElementById('types');
+		pokeTypesElement.innerText = 'タイプ: ' + types[0].type.name;
+		if (types[1]) {
+			pokeTypesElement.innerText += ',' + types[1].type.name;
+		}
 
-    const pokeIMAGE = document.getElementById("img");
-    pokeIMAGE.src = pokeIMG;
-
-    const listName = document.getElementById("name");
-    listName.innerText = "名前: " + pokeName;
-
-    const listID = document.getElementById("id");
-    listID.innerText = "ID: " + pokeID;
-
-    const listWeight = document.getElementById("weight");
-    listWeight.innerText = "重さ: " + pokeweight + "kg";
-
-    const listHeight = document.getElementById("height");
-    listHeight.innerText = "高さ: " + pokeheight + "cm";
-  } catch (error) {
-    // catch: 投げられたエラーを捕捉する
-    console.error("非同期エラー:", error.message);
-    errorMessage.innerText = "ID" + idNumber + "というポケモンは存在しません。";
-    loadingIMG.remove();
-    // return null; などのエラー後の処理
-  }
+		const pokeIMAGE = document.getElementById('img');
+		pokeIMAGE.src = sprites.front_default;
+		makeEachList('name', '名前: ', name, '');
+		makeEachList('id', 'ID: ', id, '');
+		makeEachList('weight', '重さ: ', weight, 'kg');
+		makeEachList('height', '高さ: ', height, 'm');
+		getPokeLog(id, name);
+	} catch (error) {
+		// catch: 投げられたエラーを捕捉する
+		console.error('非同期エラー:', error.message);
+		errorMessage.innerText = 'ID' + idNumber + 'というポケモンは存在しません。';
+		loadings(false);
+		// return null; などのエラー後の処理
+	}
 }
 
-async function getPokeLog() {
-  const inputNumber = document.getElementById("pokeID");
-  const idNumber = inputNumber.value; /*入力されたID数値を格納する変数*/
-  try {
-    // await: fetchが完了するまで一時停止
-    const pokeAPI = "https://pokeapi.co/api/v2/pokemon";
-    const eachID = "/" + idNumber + "/";
-    const pokeeachID =
-      pokeAPI +
-      eachID; /*それぞれのIDポケモンのデータが個別に格納されているリンク*/
-    const response = await fetch(pokeeachID);
+const getPokeLog = (pokeID, pokename) => {
+	const logList = document.getElementById('log');
+	logList.innerHTML = '';
 
-    if (!response.ok) {
-      // 失敗時、ここでエラーを投げる
-      throw new Error("HTTPエラー");
-    }
+	const logText = 'ID' + pokeID + ': ' + pokename;
+	pokeLogs.unshift(logText);
 
-    // await: JSONの解析が完了するまで一時停止
-    const data = await response.json();
-    const pokeName = data.name;
-    const pokeID = data.id;
+	initLogMessage.innerText = '';
 
-    const logList = document.getElementById("log");
-    logList.innerHTML = "";
+	for (let i = 0; i < pokeLogs.length; i++) {
+		const logNameAndID = document.createElement('li');
+		logNameAndID.innerText = pokeLogs[i];
+		logList.appendChild(logNameAndID);
+	}
+};
 
-    const logText = "ID" + pokeID + ": " + pokeName;
-    pokeLogs.unshift(logText);
+const makeEachList = (eachid, category, value, unit) => {
+	const eachList = document.getElementById(eachid);
+	eachList.innerText = category + value + unit;
+};
 
-    initLogMessage.innerText = "";
-
-    for (let i = 0; i < pokeLogs.length; i++) {
-      const logNameAndID = document.createElement("li");
-      logNameAndID.innerText = pokeLogs[i];
-      logList.appendChild(logNameAndID);
-    }
-  } catch (error) {
-    // catch: 投げられたエラーを捕捉する
-    console.error("非同期エラー:", error.message);
-    // return null; などのエラー後の処理
-  }
-}
+const loadings = (bool) => {
+	const loadingIMG = document.getElementById('loadingIMG');
+	if (bool) {
+		loadingIMG.style.display = 'block';
+		loadingIMG.src = 'https://usagif.com/wp-content/uploads/loading-96.gif';
+	} else {
+		loadingIMG.style.display = 'none';
+	}
+};
